389c58b8610e988eaf760fd189f965d49c578657
KartikBhargava <bhargava.kartik07@gmail.com>
2020-09-04 01:40:12 +0530

    HomeScreen Done

--- src/screens/HomeStackScreen.js
+++ src/screens/HomeStackScreen.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import SiteEngineerProjectScreen from './SiteEngineerProjectScreen';
 import LoginStackScreen from './LoginStackScreen';
-import HomeTabScreen from '../components/HomeBottomTab';
 import AreaWiseVendorListScreen from './AreaWiseVendorListScreen';
 import FilesScreen from './FilesScreen';
 import BookNewScreen from './BookNewScreen';
-import MaterialScreen from './BottomTabScreens/MaterialScreen';
+import InventoryScreen from './InventoryScreen';
+import HomeScreen from './HomeScreens/HomeScreen';
+import MaterialScreen from './HomeScreens/MaterialScreen';
+import TrackScreen from './HomeScreens/TrackScreen';
+import ItemInventoryScreen from './ItemInventoryScreen';
 
 const HomeStack = createStackNavigator();
 
@@ -20,12 +23,15 @@ function Login() {
 const HomeStackScreen = () => (
   <HomeStack.Navigator headerMode='none'>
     <HomeStack.Screen name="Login" component={Login} />
-    <HomeStack.Screen name="HomeScreen" component={HomeTabScreen} />
+    <HomeStack.Screen name="HomeScreen" component={HomeScreen} />
+    <HomeStack.Screen name='MaterialScreen' component={MaterialScreen} />
+    <HomeStack.Screen name='TrackScreen' component={TrackScreen}/>
     <HomeStack.Screen name="Site Engineer Project Screen" component={SiteEngineerProjectScreen} />
     <HomeStack.Screen name="AreaWiseVendorList" component={AreaWiseVendorListScreen} />
     <HomeStack.Screen name="FilesScreen" component={FilesScreen} />
     <HomeStack.Screen name="BookNewScreen" component={BookNewScreen} />
-    <HomeStack.Screen name="MaterialScreen" component={MaterialScreen} />
+    <HomeStack.Screen name="InventoryScreen" component={InventoryScreen}/>
+    <HomeStack.Screen name="ItemInventoryScreen" component={ItemInventoryScreen}/>
   </HomeStack.Navigator>
 );
 
--- src/screens/InventoryScreen.js
+++ src/screens/InventoryScreen.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { TouchableOpacity, FlatList } from 'react-native-gesture-handler'
 
-
 class InventoryScreen extends Component{
 
     constructor(props){
